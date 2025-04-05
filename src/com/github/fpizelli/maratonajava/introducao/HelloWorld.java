3ae0cf9694730e5bc6c5687a1f4cd58729fa3e9a
fpizelli <168946758+fpizelli@users.noreply.github.com>
2025-04-04 22:23:41 -0300

    Lesson 009: Java Comments

--- src/com/github/fpizelli/maratonajava/introducao/HelloWorld.java
+++ src/com/github/fpizelli/maratonajava/introducao/HelloWorld.java
@@ -1,6 +1,17 @@
 package com.github.fpizelli.maratonajava.introducao;
 
+/**
+ * Isto é um comentário <b>Javadoc</b><br>
+ * É sensato que cada método público de classe possua um comentário Javadoc
+ */
 public class HelloWorld {
+    // Isto é um comentário de uma linha
+
+    /*
+     * Isto é um comentário
+     * de múltiplas
+     * linhas
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }

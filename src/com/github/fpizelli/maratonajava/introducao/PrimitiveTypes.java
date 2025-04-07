dcdf970263607537b5f76f73ed230e5deb70e1ce
fpizelli <168946758+fpizelli@users.noreply.github.com>
2025-04-07 00:25:58 -0300

    Lesson 010: Primitive Types 2/4

--- src/com/github/fpizelli/maratonajava/introducao/PrimitiveTypes.java
+++ src/com/github/fpizelli/maratonajava/introducao/PrimitiveTypes.java
@@ -2,6 +2,17 @@ package com.github.fpizelli.maratonajava.introducao;
 
 public class PrimitiveTypes {
     public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
         int age = 24;
+        long bigNumber = 500000;
+        double wageDouble = 2000;
+        float wageFloat = 2500;
+        byte ageByte = 24;
+        short ageShort = 24;
+        boolean isHuman = true;
+        boolean isGamer = false;
+        char gender = 'M';
+
+        System.out.println("I am " + age + " years old.");
     }
 }
